1154bfb44e02deeebf85eec6b5d428ca19ad2be3
aina brenes <abrenes@nigul.cide.es>
2024-10-08 18:50:10 +0200

    xml1

--- ActvidadLibros/LlibresParser.java
+++ ActvidadLibros/LlibresParser.java
@@ -1,44 +1,53 @@
 package ActvidadLibros;
 
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
 import java.io.*;
-import java.util.*;
-import java.lang.reflect.Array;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.util.List;
 import java.util.ArrayList;
-import java.lang.reflect.Array;
 
 public class LlibresParser {
 
     public static void main(String[] args) {
         try {
-            File inputFile = new File("./ActvidadLibros/llibres.xml"); 
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            File inputFile = new File("./llibres.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = factory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
 
             doc.getDocumentElement().normalize();
-
             NodeList nList = doc.getElementsByTagName("llibre");
+            System.out.println("Numero de libros" + nList.getLength());
             List<Llibre> llibres = new ArrayList<>();
-
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
-
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element element = (Element) nNode;
 
-                    String autor = element.getElementsByTagName("autor").item(0).getTextContent();
-                    String titol = element.getElementsByTagName("titol").item(0).getTextContent();
-                    int any = Integer.parseInt(element.getElementsByTagName("any").item(0).getTextContent());
-                    String resum = element.getElementsByTagName("resum").item(0).getTextContent();
-
-                    llibres.add(new Llibre(autor, titol, any, resum));
+                    System.out.println("\nLibro id: " + element.getAttribute("id"));
+                    System.out.println("Marca: "
+                            + element.getElementsByTagName("marca").item(0).getTextContent());
+                    System.out.println("Modelo: "
+                            + element.getElementsByTagName("modelo").item(0).getTextContent());
+                    System.out.println("Cilindrada: "
+                            + element.getElementsByTagName("cilindrada").item(0).getTextContent());
+                // }
+                        String autor = element.getElementsByTagName("autor").item(0).getTextContent();
+                        String titol = element.getElementsByTagName("titol").item(0).getTextContent();
+                        int any = Integer.parseInt(element.getElementsByTagName("any").item(0).getTextContent());
+                        String resum = element.getElementsByTagName("resum").item(0).getTextContent();
+                        llibres.add(new Llibre(autor, titol, any, resum));
+                    
+                    
                 }
-            }
-
-            for (Llibre llibre : llibres) {
-                System.out.println(llibre);
+                for (Llibre llibre : llibres) {
+                    System.out.println(llibre);
+                }
+                /*factory.setIgnoringComments(true);
+            factory.setIgnoringElementContentWhitespace(true);
+            DocumentBuilder dBuilder = factory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+                 */
             }
 
         } catch (Exception e) {

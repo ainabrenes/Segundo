949702204ae2d9618017e3074b4b183732f5f33a
aina brenes <abrenes@nigul.cide.es>
2024-10-15 18:48:59 +0200

    2

--- ActvidadLibros/Libres2.java
+++ ActvidadLibros/Libres2.java
@@ -1,28 +1,83 @@
 package ActvidadLibros;
 
 import java.io.File;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class Libres2 {
 
     public static void main(String[] args) {
         try {
-            // File inputFile = new File("./llibres.xml");
-            // SAXParserFactory factory = SAXParserFactory.newInstance();
-            // parser = factory.newSAXParser();
-            // sh = new ManejadorSAX();
-            // return 0;
-
+            File inputFile = new File("llibres.xml");
             SAXParserFactory factory = SAXParserFactory.newInstance();
-
             SAXParser parser = factory.newSAXParser();
 
-            LibroHandler handler = new LibroHandler();
-            parser.parse("libros.xml", handler);
+            List<String[]> llibres = new ArrayList<>();
+
+            // Handler intern
+            DefaultHandler handler = new DefaultHandler() {
+                private String[] llibreActual = new String[4]; // [autor, titol, any, resum]
+                private StringBuilder contingut = new StringBuilder();
+                private boolean llegintContingut = false;
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    if (qName.equalsIgnoreCase("llibre")) {
+                        llibreActual = new String[4]; // Reiniciar per a un nou llibre
+                    } else if (qName.equalsIgnoreCase("autor") || 
+                               qName.equalsIgnoreCase("titol") || 
+                               qName.equalsIgnoreCase("any") || 
+                               qName.equalsIgnoreCase("resum")) {
+                        llegintContingut = true; // Començar a llegir contingut
+                        contingut.setLength(0); // Netejar contingut anterior
+                    }
+                }
+
+                @Override
+                public void endElement(String uri, String localName, String qName) throws SAXException {
+                    if (llegintContingut) {
+                        switch (qName.toLowerCase()) {
+                            case "autor":
+                                llibreActual[0] = contingut.toString();
+                                break;
+                            case "titol":
+                                llibreActual[1] = contingut.toString();
+                                break;
+                            case "any":
+                                llibreActual[2] = contingut.toString();
+                                break;
+                            case "resum":
+                                llibreActual[3] = contingut.toString();
+                                break;
+                        }
+                        llegintContingut = false; // Acabar de llegir contingut
+                    } else if (qName.equalsIgnoreCase("llibre")) {
+                        llibres.add(llibreActual); // Afegir llibre complet a la llista
+                    }
+                }
+
+                @Override
+                public void characters(char[] ch, int start, int length) throws SAXException {
+                    if (llegintContingut) {
+                        contingut.append(ch, start, length); // Afegir contingut llegit
+                    }
+                }
+            };
+
+            parser.parse(inputFile, handler);
+
+            // Imprimir els llibres
+            for (String[] llibre : llibres) {
+                System.out.printf("Autor: %s, Títol: %s, Any: %s, Resum: %s%n", llibre[0], llibre[1], llibre[2], llibre[3]);
+            }
 
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ ActvidadLibros/LibroHandler.java
@@ -0,0 +1,29 @@
+package ActvidadLibros;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+ 
+public class LibroHandler extends DefaultHandler {
+ 
+    private StringBuilder value;
+ 
+    public LibroHandler() {
+        this.value = new StringBuilder();
+    }
+ 
+    @Override
+    public void startElement(String uri, String localName,
+            String qName, Attributes attributes)
+            throws SAXException {
+ 
+        this.value.setLength(0);
+ 
+        if (qName.equals("libro")) {
+            String anio = attributes.getValue("año");
+            System.out.println("Atributo año: " + anio);
+        }
+ 
+    }
+ 
+}

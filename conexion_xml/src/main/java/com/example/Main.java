082f4980320fcc9280c5eaeaf5a61f5bf3847995
aina brenes <abrenes@nigul.cide.es>
2025-01-27 18:43:56 +0100

    a

--- conexion_xml/src/main/java/com/example/Main.java
+++ conexion_xml/src/main/java/com/example/Main.java
@@ -17,6 +17,7 @@ public class Main {
             String xmlContent = new String(Files.readAllBytes(Paths.get(xmlFilePath)));
 
             // Imprimir el contenido original del archivo XML
+            System.out.println("");
             System.out.println("Contenido del archivo XML:");
             System.out.println(xmlContent);
 
@@ -24,6 +25,7 @@ public class Main {
             JSONObject jsonObject = Xml2Json.convertirXmlAJson(xmlContent);
 
             // Imprimir el JSON convertido
+            System.out.println("");
             System.out.println("\nContenido del archivo JSON:");
             System.out.println(jsonObject.toString(4));  // La opción '4' es para una salida formateada
 
@@ -43,6 +45,7 @@ public class Main {
             String xmlConverted = Json2Xml.convertirJsonAXml(jsonFromFile);
 
             // Imprimir el XML convertido
+            System.out.println("");
             System.out.println("\nContenido del archivo XML convertido desde JSON:");
             System.out.println(xmlConverted);
 
